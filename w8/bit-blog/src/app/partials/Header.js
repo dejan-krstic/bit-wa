3fbcf4ba86b4d5a2eb9105d6ca897cae8c8e71c2
BIT Student <classroom@bgit.rs>
2018-03-26 18:23:40 +0200

    white letters

--- w8/bit-blog/src/app/partials/Header.js
+++ w8/bit-blog/src/app/partials/Header.js
@@ -5,14 +5,13 @@ const Header = (props) => {
     return (
         <nav className="navbar navbar-expand navbar-light bg-light">
             <span className="navbar-brand">BIT Blog</span>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
             <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
                 <ul className="navbar-nav">
+
                     <li className="nav-item"><Link to='/' className="nav-link" activeclassname="highlight">Home</Link></li>
                     <li className="nav-item"><Link to='/authors' className="nav-link" activeclassname="highlight">Authors</Link></li>
                     <li className="nav-item"><Link to='/about' className="nav-link" activeclassname="highlight">About</Link></li>
+                    <li className="nav-item white"><Link to='/posts/new' className="nav-link btn btn-info" activeClassName="highlight">Create new post</Link></li>
                 </ul>
             </div>
         </nav>
